503a172e1592353de9602e026e004c8e3dfdd63f
Giacomo Grassetti <giacomo.grassetti@studio.unibo.it>
2021-01-17 16:36:43 +0100

    fixed bug home.php

--- template/home.php
+++ template/home.php
@@ -1,16 +1,61 @@
 <div id="item-list" class="container-fluid mt-2">
-        <div class="col-sm-3 col-md-3">
-        <div class="item-container">
-            <img title="Ravenous Hydraa" class="item" src="img/idra_famelica.png" alt="Ravenous Hydra">
+    <div class="col-sm-12 col-md-12 row">
+        <div class="item-container col-md">
+            <img title="Ravenous Hydra" class="item" src="img/idra_famelica.png" alt="Ravenous Hydra">
             <div class="label-sale">
                 SALES
             </div>
             <div class="row">
-                <div class="col-sm col-dm">
+                <div class="col-sm col-md">
                     <p>Ravenous Hydra<br>Legendary item<br>30$</p>
                 </div>
-                <div class="col-sm col-dm d-flex justify-content-center align-self-center">
-                    <a class="add-to-cart" href="#"><i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i></a>                
+                <div class="col-sm col-md d-flex justify-content-center align-self-center">
+                    <a href="#"><i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i></a>                
+                </div>
+            </div>
+        </div>
+
+        <div class="item-container col-md">
+            <img title="Ravenous Hydra" class="item" src="img/idra_famelica.png" alt="Ravenous Hydra">
+            <div class="label-sale">
+                SALES
+            </div>
+            <div class="row">
+                <div class="col-sm col-md">
+                    <p>Ravenous Hydra<br>Legendary item<br>30$</p>
+                </div>
+                <div class="col-sm col-md d-flex justify-content-center align-self-center">
+                    <a href="#"><i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i></a>                
+                </div>
+            </div>
+        </div>
+
+        <div class="item-container col-md">
+            <img title="Ravenous Hydra" class="item" src="img/idra_famelica.png" alt="Ravenous Hydra">
+            <div class="label-sale">
+                SALES
+            </div>
+            <div class="row">
+                <div class="col-sm col-md">
+                    <p>Ravenous Hydra<br>Legendary item<br>30$</p>
+                </div>
+                <div class="col-sm col-md d-flex justify-content-center align-self-center">
+                    <a href="#"><i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i></a>                
+                </div>
+            </div>
+        </div>
+
+        <div class="item-container col-md">
+            <img title="Ravenous Hydra" class="item" src="img/idra_famelica.png" alt="Ravenous Hydra">
+            <div class="label-sale">
+                SALES
+            </div>
+            <div class="row">
+                <div class="col-sm col-md">
+                    <p>Ravenous Hydra<br>Legendary item<br>30$</p>
+                </div>
+                <div class="col-sm col-md d-flex justify-content-center align-self-center">
+                    <a href="#"><i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i></a>                
                 </div>
             </div>
         </div>

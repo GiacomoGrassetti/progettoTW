fbccc6f43de11c530ab61a361e87c7285cbbc3c9
Giacomo Grassetti <giacomo.grassetti@studio.unibo.it>
2021-01-17 15:51:53 +0100

    added template item

---
+++ template/home.php
@@ -0,0 +1,19 @@
+<div id="item-list" class="container-fluid mt-2">
+        <div class="col-sm-3 col-md-3">
+        <div class="item-container">
+            <img title="Ravenous Hydraa" class="item" src="img/idra_famelica.png" alt="Ravenous Hydra">
+            <div class="label-sale">
+                SALES
+            </div>
+            <div class="row">
+                <div class="col-sm col-dm">
+                    <p>Ravenous Hydra<br>Legendary item<br>30$</p>
+                </div>
+                <div class="col-sm col-dm d-flex justify-content-center align-self-center">
+                    <a class="add-to-cart" href="#"><i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i></a>                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+

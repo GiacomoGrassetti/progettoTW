fd9bb1d3230e0eafdb888bbbee84799f428f0f0a
Giacomo Grassetti <giacomo.grassetti@studio.unibo.it>
2021-01-27 23:36:02 +0100

    delete var_dump

--- template/base.php
+++ template/base.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<?php var_dump(unserialize($_COOKIE["cart"], ["allowed_classes" => false]));?>
 <html lang="en">
     <head>
         <meta  charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
